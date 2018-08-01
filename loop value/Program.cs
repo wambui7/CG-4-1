6e0aab268b44c83e619362431ec75e28bc7a2119
wambui7 <shiko.sheila@gmail.com>
2018-08-01 11:20:48 -0500

    commit all

---
+++ loop value/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace loop_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter the number");
+
+            string userinputstr = Console.ReadLine();
+
+            int userinputInt = int.Parse(userinputstr);
+
+            int sumOutput = 0;
+
+            for (int Loopvalue = 2; Loopvalue < 20; Loopvalue++)
+
+            {
+                Console.WriteLine(Loopvalue * userinputInt);
+
+                sumOutput += Loopvalue * userinputInt;
+
+            }
+            Console.WriteLine(sumOutput);
+            Console.ReadLine();
+        }
+    }
+}
+
+        }
+    }
+}
